b6c89c715d7bb4cc49f9c221605b7400f9d67303
Rahul k <rahuljcet95@gmail.com>
2023-12-21 13:31:35 +0530

    static changed

--- homely/settings.py
+++ homely/settings.py
@@ -137,15 +137,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'STATIC')]
+# STATICFILES_DIRS=[os.path.join(BASE_DIR,'STATIC')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    # Other directories if needed...
+]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
